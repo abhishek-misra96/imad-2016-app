56d33ef3c75e8b4470a99dd34967e3a89dee2bda
abhishek-misra96 <abhishek.misra96@gmail.com>
2016-10-09 10:35:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/ui/main.js', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function (req, res) {
-    counter = counter ++;
+    counter = counter + 1;
   res.send(counter.toString());
 });
 
