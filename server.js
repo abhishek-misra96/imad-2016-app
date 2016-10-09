59d3fa9019fb0df2bc3f67fcbd8b429c85b5ccd8
abhishek-misra96 <abhishek.misra96@gmail.com>
2016-10-09 15:40:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,9 +155,9 @@ app.get('/ui/lol.jpg', function (req, res) {
 
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
  //get the name from the request
- var name = req.params.name;
+ var name = req.query.name;
  
  
  names.push(name);
