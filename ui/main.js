5b5d46e77593174c02d0ceaeb8fdb8e760274abf
abhishek-misra96 <abhishek.misra96@gmail.com>
2016-10-09 20:49:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,3 +63,13 @@ var name = nameInput.value;
 request.send(null);
     };
 
+
+var post =  document.getElementById("postc");
+post.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState ===)
+    }
+}
+
+ 
