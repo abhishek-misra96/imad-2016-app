cb3aba828c25d7e0c05fc24c94d737b66f19e691
abhishek-misra96 <abhishek.misra96@gmail.com>
2016-10-09 16:49:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,6 +60,6 @@ submit.onclick = function(){
     var nameInput = document.getElementById("name");
 var name = nameInput.value;
    request.open('GET', 'http://abhishek-misra96.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
+
     };
 
