3193ae10909658084d67ed64d08b0bdb9ad89469
abhishek-misra96 <abhishek.misra96@gmail.com>
2016-10-09 15:37:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,6 +154,19 @@ app.get('/ui/lol.jpg', function (req, res) {
 });
 
 
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+ //get the name from the request
+ var name = req.params.name;
+ 
+ 
+ names.push(name);
+ //JSON Java Script Object Notation 
+ res.send(JSON.stringify(names));//todo
+});
+
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
